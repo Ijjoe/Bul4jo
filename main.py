5de9d9267cfa11da9def82ea336a9c8c27c59d22
Ijjoe <cijjj9220@gmail.com>
2024-04-16 10:48:16 +0900

    new

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import re
 import streamlit as st
 
 st.write("Hello world")
+st.image('./img/qrcode.jpg', caption='주소')
 st.image('./img/techstack.jpg', caption='기술스택')
-st.markdown("<img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fbul4jo.streamlit.app%2F&chs=180x180&choe=UTF-8&chld=L|2" alt="qr code"><a href="ko.qr-code-generator.com/" border="0" style="cursor:default" rel="nofollow"></a>")
