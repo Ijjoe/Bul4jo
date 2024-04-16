19ed32d94d1fc7a7bdaa5eda3ac0745840a5fe6d
Ijjoe <cijjj9220@gmail.com>
2024-04-16 13:14:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import streamlit as st
 import streamlit_shadcn_ui as ui
 
 
-with ui.card(key="card1"):
-    ui.element("span", children=["Email"], className="text-gray-400 text-sm font-medium m-1", key="label1")
-    ui.element("input", key="email_input", placeholder="Your email")
+trigger_btn = ui.button(text="Trigger Button", key="trigger_btn")
 
-    ui.element("span", children=["User Name"], className="text-gray-400 text-sm font-medium m-1", key="label2")
-    ui.element("input", key="username_input", placeholder="Create a User Name")
-    ui.element("button", text="Submit", key="button", className="m-1")
+ui.alert_dialog(show=trigger_btn, title="Alert Dialog", description="This is an alert dialog", confirm_label="OK", cancel_label="Cancel", key="alert_dialog1")
+
 
 st.write("Hello world")
 st.image('./img/qrcode.jpg', caption='주소')
