0d8ba4880b8f026245dd34ee328459b079305434
Ijjoe <cijjj9220@gmail.com>
2024-04-17 09:03:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def example(ttx):
 
 example("Test bear")    
 
-#st.write("Hello world")
-#st.image('./img/qrcode.jpg', caption='주소')
-#st.image('./img/techstack.jpg', caption='기술스택')
+st.write("불사조 2차 미니 프로젝트")
+st.image('./img/qrcode.jpg', caption='주소')
+st.image('./img/techstack.jpg', caption='기술스택')
 
 
 
