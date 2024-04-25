95240ca5e63bfce2155f824a8a1ec4d6c80def1d
Ijjoe <cijjj9220@gmail.com>
2024-04-25 17:54:40 +0900

    new

--- injun_ssi/main.py
+++ injun_ssi/main.py
@@ -1,48 +1,38 @@
 import streamlit as st 
-from langchain.llms import CTransformers
 from streamlit_mic_recorder import mic_recorder,speech_to_text
 import os
 from audio_recorder_streamlit import audio_recorder
 import sys
 import datetime
+import nemo.collections.asr as nemo_asr
+import copy
+import librosa
+from st_audiorec import st_audiorec
+import soundfile as sf
+
+asr_model = nemo_asr.models.ASRModel.from_pretrained("eesungkim/stt_kr_conformer_transducer_large")
+
+
+def convert_sample_rate(input_file, output_file, target_sr=16000):
+    # 오디오 파일을 원본 샘플 레이트로 로드합니다.
+    audio, sr = librosa.load(input_file, sr=None)
+    # 샘플 레이트를 16000Hz로 변경합니다.
+    audio_resampled = librosa.resample(audio, orig_sr=sr, target_sr=target_sr)
+    # 변경된 샘플 레이트의 오디오 파일을 저장합니다.
+    sf.write(output_file, audio_resampled, target_sr)
+
+# 사용 예
+convert_sample_rate('/content/fisu.wav', '/content/output_16.wav')
 
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(working_dir)
 
+wav_audio_data = st_audiorec()
 
-audio_bytes = audio_recorder()
-if audio_bytes:
-    st.audio(audio_bytes,format="audio/wav")
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    #with open(f"audio_{timestamp}.wav", "wb") as f:
-        #f.write(audio_bytes)
+if wav_audio_data is not None:
+    st.audio(wav_audio_data, format='audio/wav')
 
 
-# audio = mic_recorder(
-#     start_prompt="Start recording",
-#     stop_prompt="Stop recording",
-#     just_once=False,
-#     use_container_width=False,
-#     format="wav",
-#     callback=None,
-#     args=(),
-#     kwargs={},
-#     key=None
-# )     
-            
-# llm =CTransformers(
-#    model = "llama-2-7b-chat.ggmlv3.q2_K.bin",
-#    model_type ="llama"
-# )
 
-# st.title('인공지는 시인')
-
-# con = st.text_input('시의 주제를 제시해주세요')
-
-# if st.button('시 작성 요청하기'):
-#   with st.spinner('시 작성 중 ...'):
-#     res = llm.predict("write a poem about " + con + ":")
-#     st.write(res)
-    
 # streamlit run main.py
